b9ea168b6501d8769f6bfb9e22dcc5e61461ec56
Hiep Phan <64212925+phanhaihiep@users.noreply.github.com>
2022-02-17 03:58:01 +0100

    bai tap java

---
+++ buoiso7/main.js
@@ -0,0 +1,43 @@
+let fullName ="Phan Hai Hiep"
+
+alert(fullName)
+
+// if (false) {
+//     console.log('dieu kien dung');      
+// } else {    
+//     console.log('dieu kien sai');
+    
+// }
+
+let fullName2 ="Phan Hai Hiep";
+alert(fullName2);
+
+let message ="Học về built-in functions trong JS";
+let warnMessage ="Đây là message cảnh báo";
+let errorMessage ="Đây là message lỗi";
+
+console.log(message);
+console.warn(warnMessage);
+console.error(errorMessage);
+
+// var a = '15' - 5;
+// console.log(a)
+
+
+let name2 ="Hiep"
+let full ="Phan"
+console.log(name2 + ' '+ full)
+
+
+function run(age) {
+    if (age >=18) {
+        return("can vote")          
+    } else {    
+        return ("can not vote")
+        
+    }
+}
+
+let n = 11;
+let flag = true;
+
---
+++ javascript_HW/basic_java.js
@@ -0,0 +1,105 @@
+// 1.Viết chương trình in ra các số từ 1 đến n?
+function displayNumber(n) {
+  for (let i = 1; i<=n; i++) {
+    document.write(i + ' ')
+  }
+}
+
+displayNumber(10);
+document.write("</br>")
+
+
+// 2.Viết chương trình in ra các số lẻ từ 1 đến n?
+function displayOddNumber(n) {
+  for (let i = 1; i<=n; i++) {
+      if (i%2==1)
+      document.write(i + " ")
+
+  }
+}
+
+displayOddNumber(15);
+document.write("</br>");
+
+
+//3. Viết chương trình in ra các số chẳn từ 1 đến n?
+function displayEvenNumber(n) {
+  for (let i = 1; i<=n; i++) {
+    if (i%2==0)
+      document.write(i + " ")
+  }
+}
+
+displayEvenNumber(20);
+document.write("</br>")
+
+
+//4. Viết chương trình in ra số là nguyên tố trong khoảng 1 đến n?
+function checkPrime(n) {
+  let flag= true;
+  if (n<2) {
+    flag = false;
+  } else if (n ==2) {
+    flag = true
+  } else if (n% 2== 0){
+    flag = false;
+  } else {
+    for (let i = 3; i <= Math.sqrt(n); i +=2) {
+      if (n % i ==0) {
+        flag= false;
+        break;
+      }
+    }
+  }
+
+  if (flag==true){
+    document.write(n + " là số nguyên tố")
+  } else {
+    document.write(n + " không là số nguyên tố")
+  }
+}
+
+checkPrime(13)
+document.write("</br>")
+checkPrime(20)
+document.write("</br>")
+
+// 5. Viết chương trình tính tổng từ 1 đến n?
+
+function total(n) {
+  let sum = 0
+  for (let i = 1; i<=n;i++){
+    sum +=i;
+  }
+  document.write("Tổng từ 1 đến "+n+" là: "+sum)
+
+}
+
+total(3)
+document.write("</br>")
+
+// 6.  Viết chương trình tính tổng bình phương của các số từ 1 đến n?
+function totalOfSquare(n) {
+  let sum = 0;
+  for (let i = 1; i<=n; i++) {
+    sum += i *i;
+  }
+  document.write("Tổng bình phương của "+n+ " là: "+sum)
+}
+
+totalOfSquare(3)
+document.write("</br>")
+
+
+//7. Tính tổng các số lẻ trong khoảng 1 đến n?
+function totalOddNumber(n) {
+  let sum = 0;
+  for ( let i = 1; i<=n; i++) {
+    if (i%2 !==0) {
+      sum +=i;
+    }
+  }
+  document.write("Tổng các số lẻ trong khoảng 1 đến "+n+ " là:" + sum)
+}
+
+totalOddNumber(5)
